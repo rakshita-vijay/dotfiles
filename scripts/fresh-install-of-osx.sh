531f8775fc90e316a2c9ec6d1622c30f7a5c9e09
Rakshita Vijay <rakshita.vijay1@users.noreply.github.com>
2024-12-21 20:46:53 +0530

    Normalized for dotfiles repo on Mon Dec 23 23:09:13 IST 2024

--- scripts/fresh-install-of-osx.sh
+++ scripts/fresh-install-of-osx.sh
@@ -304,7 +304,9 @@ fi
 section_header 'Setting up login items'
 app_list=(
   'AlDente.app'
+  'Bitwarden.app'
   'Clocker.app'
+  'Command X.app'
   'Ice.app'
   'Itsycal.app'
   'KeepingYouAwake.app'
